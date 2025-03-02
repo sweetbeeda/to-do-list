5e217c2a52dc88e3ca90858aa52ee514ae381201
sweetbeeda <yashwanth2453@gmail.com>
2025-03-02 18:20:05 +0530

    fix : created a problem of creating a list in the website

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const button = document.getElementById(`btn`);
+
 button.addEventListener(`click`, () => {
-  const userInput = document.getElementById(`user-input`);
-  const listContainer =document.getElementById(`list-container`)
-  const list = document.createElement(`li`);
-  const textContent = document.createTextNode(userInput.value);
-  list.className = `list-item`;
-  list.append(textContent);
-  listContainer.append(list);
+  const userInput = document.getElementById(`user-input`).value;
+
+  const listContainer = document.getElementById(`list-container`);
+
+  const listItems = document.createElement(`li`);
+
+  const divEL = document.createTextNode(userInput);
+
+  const divIcon = document.createElement(`i`);
+
+  listItems.setAttribute(`onclick`, `removeItem(event)`);
+
+  divIcon.classList = "fa fa-xmark";
+
+  listItems.classList = `list-item`;
+
+  listItems.append(divEL, divIcon);
+
+  listContainer.append(listItems);
 });
+
+function removeItem(event) {
+  let existingList = event.target.parentNode;
+  existingList.remove();
+}
